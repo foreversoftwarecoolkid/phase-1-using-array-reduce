36579150f17addfc7aac238db6eda0010a434ff8
Henry <henry.bogita@student.moringaschool.com>
2023-12-21 14:34:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+
+const batteryBatches = [
+    [
+       { type: 'alkaline', capacity: 2000 },
+       { type: 'alkaline', capacity: 1500 },
+    ],
+    [
+       { type: 'lithium', capacity: 12000 },
+    ],
+   ];
+
+   const reducer = (batteryBatches) => {
+    const totalBatteries = batteryBatches.reduce((accumulator, batteryBatch) => {
+       const batteryCount = batteryBatch.reduce((batchAccumulator, battery) => {
+         return batchAccumulator + 1;
+       }, 0);
+   
+       return accumulator + batteryCount;
+    }, 0);
+   
+    return totalBatteries;
+   };
+   
+   const totalBatteries = reducer(batteryBatches);
+   console.log('Total Batteries:', totalBatteries);
+
